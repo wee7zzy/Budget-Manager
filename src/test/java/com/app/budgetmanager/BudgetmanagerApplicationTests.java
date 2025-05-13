983382b4103fda3667ddf5b67f2d793a2f1e1f1d
yassine ELANIQ <yassaniq@gmail.com>
2025-05-13 10:00:45 +0100

    initialisation du projet

---
+++ src/test/java/com/app/budgetmanager/BudgetmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.budgetmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetmanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
